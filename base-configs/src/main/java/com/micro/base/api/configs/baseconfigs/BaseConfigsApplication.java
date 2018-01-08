b1fea9562d25e84437a3d30aeedc9e0b5a9d4d92
18048529172 <315547042@qq.com>
2018-01-08 22:20:21 +0800

    11111

---
+++ base-configs/src/main/java/com/micro/base/api/configs/baseconfigs/BaseConfigsApplication.java
@@ -0,0 +1,12 @@
+package com.micro.base.api.configs.baseconfigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseConfigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseConfigsApplication.class, args);
+	}
+}
---
+++ base-configs/src/test/java/com/micro/base/api/configs/baseconfigs/BaseConfigsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.micro.base.api.configs.baseconfigs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseConfigsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
